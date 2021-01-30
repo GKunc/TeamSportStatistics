ba2e2a89e085cfa059c4b4e62a222e79043cb163
Grzegorz Kunc <grzegorz.kunc@relativity.com>
2021-01-30 14:31:14 +0100

    Init Heroku

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-const path = require('path');
-const express = require('express');
-const app = express();
+var express = require("express");
+var bodyParser = require("body-parser");
+// var mongodb = require("mongodb");
+// var ObjectID = mongodb.ObjectID;
+
+// var CONTACTS_COLLECTION = "contacts";
+
+var app = express();
+app.use(bodyParser.json());
+
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/TeamSportStatistics'));
+// app.use(express.static(__dirname + '/dist/MY_APP_NAME'));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/TeamSportStatistics/index.html'));
-});
+// app.get('/*', function(req, res) {
+  // res.sendFile(path.join(__dirname + '/dist/MY_APP_NAME/index.html'));
+// });
 
 // default Heroku port
 app.listen(process.env.PORT || 5000);
