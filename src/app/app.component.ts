5984ba946ae93039f78240fa1570a9624b4919bf
Grzegorz Kunc <grzegorz.kunc@relativity.com>
2021-01-30 11:55:38 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TeamSportStatistics';
+  flag = 0;
+  swapText(): void {
+    const text = document.getElementById('text');
+
+    if (text)
+      if(this.flag % 3 === 0)
+        text.innerHTML = "Raz!";
+      else if(this.flag % 3 === 1)
+        text.innerHTML = "Dwa!";
+      else if(this.flag % 3 === 2)
+        text.innerHTML = "Trzy!";
+
+    this.flag++;
+  }
 }
